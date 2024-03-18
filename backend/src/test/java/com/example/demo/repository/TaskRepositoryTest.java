2fd72ed150ec36a5a76abd70f501c806eac1e556
Necron-Lab <119136960+Necron-Lab@users.noreply.github.com>
2024-03-18 21:06:54 +0100

    TaskRepositoryTest Anpassung

--- backend/src/test/java/com/example/demo/repository/TaskRepositoryTest.java
+++ backend/src/test/java/com/example/demo/repository/TaskRepositoryTest.java
@@ -1,31 +1,19 @@
 package com.example.demo.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import com.example.demo.model.Task;
 
 @DataJpaTest
 public class TaskRepositoryTest {
-
-        @Autowired
-        private TestEntityManager entityManager;
         
         @Autowired
         private TaskRepository taskRepository;
-
-        @Test
-        public void testSetup() {
-            assertNotNull(entityManager);
-        }
         
         @Test
         public void testAddFind() {
@@ -33,9 +21,9 @@ public class TaskRepositoryTest {
             Task task = new Task();
             task.setTaskdescription(taskDescription);
             taskRepository.save(task);
-            Optional<Task> result = taskRepository.findByTaskdescription(taskDescription);
+            Task result = taskRepository.findByTaskdescription(taskDescription);
 
-            assertTrue(result.isPresent());
-            assertEquals(taskDescription, result.get().getTaskdescription());
+            assertTrue(result != null);
+            assertEquals(taskDescription, result.getTaskdescription());
         }
 }
